01b58023077fc4a8a00d1b0652846fc3d38d16ce
Austin Putman <austin@radicaldesigns.org>
2008-09-06 19:01:48 -0700

    edit article and add article buttons converted

--- public/javascripts/article_inset.js
+++ public/javascripts/article_inset.js
@@ -8,8 +8,28 @@ RD.article_inset = function( extension ) {
     },
     cancel: function( ) {
       $(this).hide(1200).prev('.description').show();
-      $('.remote_content', this).html('');
-      $(this).parents('#add-article').addClass('inactive');
+      $(this).queue( function() { 
+        $('.remote-content', this).html('');
+        $(this).parents('#add-article').addClass('inactive');
+        $(this).dequeue();
+      } );
+      return false;
+    },
+    show_form: function() {
+      $(this).prev('.description').hide('blind');
+      $(this).show('blind', {}, 2000 );
+      RD.ui.initialize();
+    },
+    edit: function() {
+      if( $(this).is(':visible') ) { return $(this).fn('cancel'); }
+      var scope = this;
+      $( '.remote_content', this).load( $('.edit', this).attr('href'), function() { $(scope).fn('show_form'); } );
+    },
+    new: function() {
+      if( $(this).is(':visible') ) { return $(this).fn('cancel'); }
+      $(this).parents('#add-article').removeClass('inactive');
+      var scope = this;
+      $( '.remote_content', this).load( $('#add-article-button').attr('href'), function() { $(scope).fn('show_form'); } );
     }
   }, extension );
   return self;
